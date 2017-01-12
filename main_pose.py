604015a34d357dc22d5ebed4e5ea306536ae7be8
Engineering-Course <gk12330088@gmail.com>
2017-01-12 16:54:30 +0800

    heatmap

--- main_pose.py
+++ main_pose.py
@@ -14,11 +14,11 @@ parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of e
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=4, help='# images in batch')
 parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
 parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
-parser.add_argument('--fine_size', dest='fine_size', type=int, default=256, help='then crop to this size')
+parser.add_argument('--fine_size', dest='fine_size', type=int, default=64, help='then crop to this size')
 parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
 parser.add_argument('--ndf', dest='ndf', type=int, default=64, help='# of discri filters in first conv layer')
 parser.add_argument('--input_nc', dest='input_nc', type=int, default=3, help='# of input image channels')
-parser.add_argument('--output_nc', dest='output_nc', type=int, default=1, help='# of output image channels')
+parser.add_argument('--output_nc', dest='output_nc', type=int, default=16, help='# of output image channels')
 parser.add_argument('--niter', dest='niter', type=int, default=200, help='# of iter at starting learning rate')
 parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for adam')
 parser.add_argument('--beta1', dest='beta1', type=float, default=0.5, help='momentum term of adam')
@@ -34,7 +34,7 @@ parser.add_argument('--serial_batch_iter', dest='serial_batch_iter', type=bool, 
 parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='./checkpoint', help='models are saved here')
 parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
 parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
-parser.add_argument('--L1_lambda', dest='L1_lambda', type=float, default=100.0, help='weight on L1 term in objective')
+parser.add_argument('--L2_lambda', dest='L2_lambda', type=float, default=0.1, help='weight on L1 term in objective')
 
 args = parser.parse_args()
 
@@ -49,7 +49,8 @@ def main(_):
     with tf.Session() as sess:
         model = pix2pix(sess, image_size=args.fine_size, batch_size=args.batch_size,
                         output_size=args.fine_size, dataset_name=args.dataset_name,
-                        checkpoint_dir=args.checkpoint_dir, sample_dir=args.sample_dir)
+                        checkpoint_dir=args.checkpoint_dir, sample_dir=args.sample_dir,
+                        L2_lambda = args.L2_lambda)
 
         if args.phase == 'train':
             model.train(args)
--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import scipy.misc
 import numpy as np
 import cv2
 from time import gmtime, strftime
+from scipy.stats import multivariate_normal
 
 pp = pprint.PrettyPrinter()
 
@@ -41,46 +42,50 @@ def load_image(image_path):
 #---------------------------------
 # new added function for lip dataset
 def load_lip_data(image_id, flip=False, is_test=False):
+    fine_size=64
     image_id = image_id[:-1] 
     image_path = './datasets/human/masks/{}.png'.format(image_id)
-    label_path = './datasets/human/pose/{}.png'.format(image_id)
     img_A = scipy.misc.imread(image_path).astype(np.float)
-    img_B = scipy.misc.imread(label_path).astype(np.int)
-    # print image_id
-    # print img_B.shape
-    img_A, img_B = preprocess_lip_A_and_B(img_A, img_B, flip=False, is_test=False)
+    rows = img_A.shape[0]
+    cols = img_A.shape[1]
+    img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
+    img_B = np.zeros((fine_size, fine_size, 16), dtype=np.float64)
+    with open('./datasets/human/pose/{}.txt'.format(image_id), 'r') as f:
+        lines = f.readlines()
+    points = lines[0].split(',')
+    for idx, point in enumerate(points):
+        if idx % 2 == 0:
+            c_ = int(point)
+            c_ = min(c_, cols-1)
+            c_ = max(c_, 0)
+            c_ = int(fine_size * 1.0 * c_ / cols)
+        else:
+            r_ = int(point)
+            r_ = min(r_, rows-1)
+            r_ = max(r_, 0)
+            r_ = int(fine_size * 1.0 * r_ / rows)
+            if c_ + r_ == 0:
+                continue
+            var = multivariate_normal(mean=[r_, c_], cov=5)
+            for i in xrange(fine_size):
+                for j in xrange(fine_size):
+                    img_B[i, j, int(idx / 2)] = var.pdf([i, j]) * 10.0
 
     img_A = img_A/127.5 - 1.
-
-    img_AB = np.concatenate((img_A, img_B[..., np.newaxis]), axis=2)
+    # print img_A.shape
+    # print img_B.shape
+    img_AB = np.concatenate((img_A, img_B), axis=2)
     # img_AB shape: (fine_size, fine_size, input_c_dim + output_c_dim)
     return img_AB
 
 #------------------------------------------------------------
-def preprocess_lip_A_and_B(img_A, img_B, load_size=286, fine_size=256, flip=False, is_test=False):
-    if is_test:
-        img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
-        img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
-    else:
-        load_size = fine_size
-        img_A = scipy.misc.imresize(img_A, [load_size, load_size])
+def preprocess_lip_A_and_B(img_A, fine_size=256, flip=False, is_test=False):
 
-        reimg_B = np.zeros((load_size, load_size), dtype=np.int)
-        for i in xrange(img_B.shape[0]):
-            for j in xrange(img_B.shape[1]):
-                if img_B[i,j] != 0:
-                    reimg_B[int(load_size*1.0/img_B.shape[0]*i), 
-                            int(load_size*1.0/img_B.shape[1]*j)] = img_B[i,j]
-                    # print ('i: %d, j: %d, id: %d'%(i, j, img_B[i,j]))
-
-        # h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        # w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        # img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
-        # reimg_B = reimg_B[h1:h1+fine_size, w1:w1+fine_size]
-
-        # if flip and np.random.random() > 0.5:
-        #     img_A = np.fliplr(img_A)
-        #     reimg_B = np.fliplr(reimg_B)
+    for i in xrange(img_B.shape[0]):
+        for j in xrange(img_B.shape[1]):
+            if img_B[i,j] != 0:
+                reimg_B[int(fine_size*1.0/img_B.shape[0]*i), 
+                        int(fine_size*1.0/img_B.shape[1]*j)] = img_B[i,j]
 
     return img_A, reimg_B
 
@@ -106,11 +111,27 @@ def preprocess_A_and_B(img_A, img_B, load_size=286, fine_size=256, flip=True, is
 # -----------------------------
 
 # new added function for lip dataset
-def save_lip_images(images, batch_size, batch_idx, preffix, sample_files):
+def save_lip_images(images, batch_size, sample_files, batch_idx=0):
     # images = inverse_transform(images)
     for i, image in enumerate(images):
-        path = './test/{}_{}.png'.format(preffix, sample_files[batch_size * batch_idx + i][:-1])
-        cv2.imwrite(path, np.squeeze(image))
+        img_id = sample_files[batch_size * batch_idx + i][:-1]
+        image_path = './datasets/human/masks/{}.png'.format(img_id)
+        img_A = scipy.misc.imread(image_path).astype(np.float)
+        rows = img_A.shape[0]
+        cols = img_A.shape[1]
+        with open('./test/pose/{}.txt'.format(img_id), 'w') as f:
+            for p in xrange(image.shape[2]):
+                channel_ = image[:,:,p]
+                if (np.abs(channel_.max()) < 1e-5):
+                    r_ = 0
+                    c_ = 0
+                else:
+                    r_, c_ = np.where(channel_ == channel_.max())
+                    r_ = r_ * rows * 1.0 / channel_[0]
+                    c_ = c_ * cols * 1.0 / channel_[1]
+                f.write(str(r_) + ' ' + str(c_) + ' ')
+        # path = './test/{}_{}.png'.format(preffix, sample_files[batch_size * batch_idx + i][:-1])
+        # cv2.imwrite(path, np.squeeze(image))
         # scipy.misc.imsave(path, np.squeeze(image))
 
 def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale = False):
